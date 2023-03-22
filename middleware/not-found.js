749509b93af5f5db4a39217907dbdad1fbe4dab9
streetcoder <streetkoder@gmail.com>
2023-03-22 17:00:27 -0600

    env file added

---
+++ middleware/not-found.js
@@ -0,0 +1,3 @@
+const notFoundMiddleWare = (req, res) => res.status(404).send('Route does not exist')
+
+export default notFoundMiddleWare
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('server running...')
+import express from "express";
+const app = express();
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+// middleware
+import notFoundMiddleWare from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+app.get("/", (req, res) => {
+  throw new Error('error')
+  res.send("Welcome!");
+});
+
+app.use(notFoundMiddleWare)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
