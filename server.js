cc64d1a3793e14820717f0ac1ea2b505ea73adb5
streetcoder <streetkoder@gmail.com>
2023-03-22 23:27:54 -0600

    auth controller and jobs controller created

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 import express from "express";
 const app = express();
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
+// db and authenticateUser
+import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
 
 // middleware
 import notFoundMiddleWare from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  throw new Error('error')
   res.send("Welcome!");
 });
 
-app.use(notFoundMiddleWare)
-app.use(errorHandlerMiddleware)
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
