0bf5e00927f26b1c533f0827b2f8918b66cb85ff
streetcoder <streetkoder@gmail.com>
2023-03-23 23:00:38 -0600

    missing field error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,10 +3,14 @@ import {StatusCodes} from 'http-status-codes'
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err)
     const defaultError = {
-        statusCodes:StatusCodes.INTERNAL_SERVER_ERROR,
+        statusCode:StatusCodes.INTERNAL_SERVER_ERROR,
         msg: 'Something went wrong, try again later',
     }
-    res.status(500).json({msg: err})    
+    if (err.name === 'ValidationError') {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = err.message
+    }
+    res.status( defaultError.statusCode).json({ msg: defaultError.msg })    
 }
 
 export default errorHandlerMiddleware
