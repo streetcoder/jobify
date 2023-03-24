4911fe83c609bfff07d0c259620c78b4871df2f7
streetcoder <streetkoder@gmail.com>
2023-03-24 08:21:42 -0600

    error-handler supposed to be merged

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,10 +3,10 @@ import {StatusCodes} from 'http-status-codes'
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err)
     const defaultError = {
-        statusCodes:StatusCodes.INTERNAL_SERVER_ERROR,
+        statusCode:StatusCodes.INTERNAL_SERVER_ERROR,
         msg: 'Something went wrong, try again later',
     }
-    res.status(500).json({msg: err})    
+    res.status(defaultError.statusCode).json({msg: err})    
 }
 
 export default errorHandlerMiddleware
