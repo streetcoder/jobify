532ea6eaf1051a54f6d737f92c4e831eb4a85d3b
streetcoder <streetkoder@gmail.com>
2023-03-24 16:54:46 -0600

    JWT setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,33 @@
 import User from "../models/User.js";
-import {StatusCodes} from 'http-status-codes'
-import {BadRequestError} from '../errors/index.js'
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
 
-const register = async (req, res) => {  
-    const {name, email, password} = req.body
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
 
-    if(!name || !email || !password){
-        throw new BadRequestError('Please provide all values')
-    }
-    const userAlreadyExists = await User.findOne({email})
-    if(userAlreadyExists){
-        throw new BadRequestError('Email already in use')
-    }
-
-    const user = await User.create({name, email, password});
-    const token = user.createJWT()
-    res.status(StatusCodes.OK).json({ user, token });
-}
+  const user = await User.create({ name, email, password });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.OK)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+      location: user.location,
+    });
+};
 
 const login = async (req, res) => {
   res.send("login user");
@@ -27,4 +37,3 @@ const updateUser = async (req, res) => {
 };
 
 export { register, login, updateUser };
- 
