a8004326e896ee1c7b5a6d1ece31152b3a33bed7
streetcoder <streetkoder@gmail.com>
2023-03-25 01:29:10 -0600

    setup proxy

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,21 @@
-import Error from "./Error"
+import { useEffect } from 'react'
 
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      // const response = await fetch('/data.json')
+      const response = await fetch('/api/v1')
+      const data = await response.json()
+      console.log(data)      
+    } catch (error) {
+      console.log(error)      
+    }
+  }
+  useEffect( () => {
+    fetchData()
+  }, [] )
   return (
-    <h1>Dashboard</h1>
+    <h1>Dashboard Page</h1>
   )
 }
 
