4e9ea940f5c7914f4d83775d4887323218cc5a69
streetcoder <streetkoder@gmail.com>
2023-03-29 15:50:28 -0600

    profile update functionality updated

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -65,7 +65,7 @@ const AppProvider = ({ children }) => {
     (error) => {
       console.log(error.response);
       if (error.response.status === 401) {
-        console.log("AUTH ERROR");
+        logoutUser();
       }
       return Promise.reject(error);
     }
@@ -177,9 +177,14 @@ const AppProvider = ({ children }) => {
       });
       addUserToLocalStorage({ user, location, token });
     } catch (error) {
-      dispatch({type: UPDATE_USER_ERROR, payload:{msg:error.response.data.msg}})
+      if(error.response.status !== 401){
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: error.response.data.msg },
+        });
+      }
     }
-    clearAlert()
+    clearAlert();
   };
 
   return (
