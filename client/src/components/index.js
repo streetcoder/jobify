1def79b407cb7a8102c8db077ebaf51494032cc7
streetcoder <streetkoder@gmail.com>
2023-03-30 10:52:12 -0600

    FormRowSelect is created

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,8 @@
 import FormRow from "./FormRow";
+import FormRowSelect from "./FormRowSelect";
 import Logo from "./Logo";
 import Alert from "./Alert";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
-
-export { FormRow, Logo, Alert, Navbar, SmallSidebar, BigSidebar };
+export { FormRow, Logo, Alert, Navbar, SmallSidebar, BigSidebar, FormRowSelect };
--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,4 +1,4 @@
-import { FormRow, Alert } from "../../components";
+import { FormRow, Alert, FormRowSelect } from "../../components";
 import { useAppContext } from "../../context/appContext";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
@@ -57,27 +57,23 @@ const AddJob = () => {
             value={jobLocation}
             handleChange={handleJobInput}
           ></FormRow>
-          {/* job type */}
-          <div className="form-row">
-            <label htmlFor="jobType" className="form-label">
-              job type
-            </label>
-            <select
-              name="jobType"
-              value={jobType}
-              onChange={handleJobInput}
-              className="form-select"
-            >
-              {jobTypeOptions.map((itemValue, index) => {
-                return (
-                  <option key={index} value={itemValue}>
-                    {itemValue}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
           {/* job status */}
+          <FormRowSelect
+            name="status"
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+          {/* job type */}
+
+          <FormRowSelect
+            name="jobType"
+            labelText='job type'
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
+
           <div className="btn-container">
             <button
               type="submit"
