49bd31f7fb71d0c32345323dd73c90ef9468622b
streetcoder <streetkoder@gmail.com>
2023-04-01 18:35:36 -0600

    setup check permission for updating job by user

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -5,6 +5,7 @@ import {
   NotFoundError,
   UnAuthenticatedError,
 } from "../errors/index.js";
+import checkPermissions from "../utils/checkPermissions.js";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -33,6 +34,12 @@ const updateJob = async (req, res) => {
   if (!job) {
     throw new NotFoundError(`No job with id : ${jobId}`);
   }
+  // check permission
+  console.log(typeof req.user.userId)
+  console.log(typeof job.createdBy)
+
+  checkPermissions(req.user, job.createdBy)
+
   const updatedJob = await Job.findByIdAndUpdate({ _id: jobId }, req.body, {
     new: true,
     runValidators: true,
