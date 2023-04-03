bb54c6652fd63efaf208d1f273973e27ee166ebe
streetcoder <streetkoder@gmail.com>
2023-04-03 17:09:12 -0600

    refactors monthly applications

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -7,6 +7,7 @@ import {
 } from "../errors/index.js";
 import checkPermissions from "../utils/checkPermissions.js";
 import mongoose from "mongoose";
+import moment from "moment";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -94,6 +95,17 @@ const showStats = async (req, res) => {
     },
     { $limit: 6 },
   ]);
+
+  monthlyApplications = monthlyApplications.map((item) => {
+    const{
+      _id: {year, month},
+      count,
+    } = item
+    const date = moment().month(month - 1).year(year).format('MMM Y')
+    return {date, count }
+  })
+  .reverse()
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
