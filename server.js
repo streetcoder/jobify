a8e61dac65eff4e2dbe5eba25ddd50fbdbbca8bf
streetcoder <streetkoder@gmail.com>
2023-04-06 14:40:35 -0600

    production configuration ready

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ dotenv.config();
 import 'express-async-errors'
 import morgan from "morgan";
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 // db and authenticateUser
 import connectDB from "./db/connect.js";
 
@@ -20,8 +24,12 @@ import authenticateUser from './middleware/auth.js';
 if(process.env.NODE_ENV !== 'production'){
   app.use(morgan('dev'))
 }
-app.use(express.json())
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
+app.use(express.json())
 app.get("/", (req, res) => {
   res.json({ msg: 'Welcome!'});
 });
@@ -31,6 +39,10 @@ app.get("/api/v1", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__driname, './client/build', index.html))
+})
 
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleware);
