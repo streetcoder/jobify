b41af995f9d24711ebdf565eb77530b08371b060
streetcoder <streetkoder@gmail.com>
2023-04-09 22:21:32 -0600

    refactor auth middleware and response

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,57 +16,54 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
-  attachCookies({res, token})
-  res
-    .status(StatusCodes.OK)
-    .json({
-      user: {
-        email: user.email,
-        lastName: user.lastName,
-        location: user.location,
-        name: user.name,
-      },
-      token,
+  attachCookies({ res, token });
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
       location: user.location,
-    });
+      name: user.name,
+    },
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body
-  if(!email || !password){
-    throw new BadRequestError('Please provide all values')
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all values");
   }
-  const user = await User.findOne({ email }).select('+password')
-  if(!user){
-    throw new UnAuthenticatedError('Invalid Credential')
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new UnAuthenticatedError("Invalid Credential");
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
-  if(!isPasswordCorrect){
-    throw new UnAuthenticatedError('Invalid Credential')
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid Credential");
   }
-  const token = user.createJWT()
-  user.password = undefined
-  attachCookies({res, token})
+  const token = user.createJWT();
+  user.password = undefined;
+  attachCookies({ res, token });
 
-  res.status(StatusCodes.OK).json({ user, token, location: user.location });
+  res.status(StatusCodes.OK).json({ user, location: user.location });
 };
 const updateUser = async (req, res) => {
-  const { email, name, lastName, location } = req.body
-  if(!email || !name || !lastName || !location){
-    throw new BadRequestError('Please provide all values')
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
   }
-  const user = await User.findOne({_id:req.user.userId})
-  user.email = email
-  user.name = name
-  user.lastName = lastName
-  user.location = location
-  await user.save()
+  const user = await User.findOne({ _id: req.user.userId });
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+  await user.save();
 
-  const token = user.createJWT()
-  attachCookies({res, token})
+  const token = user.createJWT();
+  attachCookies({ res, token });
 
-  res.status(StatusCodes.OK).json({ user, token, location: user.location });
+  res.status(StatusCodes.OK).json({ user, location: user.location });
 };
 
 export { register, login, updateUser };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,12 @@
 import jwt from 'jsonwebtoken';
 import { UnAuthenticatedError} from "../errors/index.js"
 
-const auth = async (req, res, next) => {
-    console.log(req.cookies);
-    const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer')){
+const auth = async (req, res, next) => {    
+    const token = req.cookies.token
+    if(!token){
         throw new UnAuthenticatedError('Authentication Invalid')
     }
-    const token = authHeader.split(' ')[1]
+    
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         const testUser = payload.userId === '64304e5419e4315875ec0788'
