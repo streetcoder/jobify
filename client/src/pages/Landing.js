6368717aa636d0851e05a3a81468666819db2215
streetcoder <streetkoder@gmail.com>
2023-04-10 18:50:27 -0600

    landing page restricted to loggedin user

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,29 +1,41 @@
-import main from '../assets/images/main.svg'
-import Wrapper from '../assets/wrappers/LandingPage'
-import { Logo } from '../components'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link, Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
 
 const Landing = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
+    <React.Fragment>
+      {user && <Navigate to='/' />}
+      <Wrapper>
         <nav>
-           <Logo /> 
+          <Logo />
         </nav>
-        <div className='container page'>
-            {/* info */}
-            <div className='info'>
-                <h1>
-                    job <span>tracking</span> app
-                </h1>
-                <p>
-                I'm baby big mood vice microdosing wayfarers glossier blue bottle. Hot chicken DIY truffaut gatekeep, jean shorts Brooklyn hoodie. Sriracha mumblecore venmo cupping, leggings palo santo ramps. 90's hoodie irony enamel pin pour-over tattooed. Fingerstache grailed subway tile man braid gastropub.
-                </p>
-                <Link to='/register' className='btn btn-hero'>Login/Register</Link>
-            </div>
-            <img src={main} alt='job hunt' className='img main-img' />
+        <div className="container page">
+          {/* info */}
+          <div className="info">
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              I'm baby big mood vice microdosing wayfarers glossier blue bottle.
+              Hot chicken DIY truffaut gatekeep, jean shorts Brooklyn hoodie.
+              Sriracha mumblecore venmo cupping, leggings palo santo ramps. 90's
+              hoodie irony enamel pin pour-over tattooed. Fingerstache grailed
+              subway tile man braid gastropub.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
         </div>
-    </Wrapper>    
-  )
-}
+      </Wrapper>
+    </React.Fragment>
+  );
+};
 
-export default Landing
+export default Landing;
